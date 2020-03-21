ad935379c729217873a332fb23f26c7826a2bf61
Milan <milan_susa@hotmail.com>
2020-03-21 15:55:40 +0100

    Add Prediction model and function for renaming images.

--- inference_api/models.py
+++ inference_api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import os
+import datetime
 
-# Create your models here.
+
+def rename_image(path):
+    def wrapper(instance, filename):
+        filename = f'{datetime.datetime.now().today()}_{filename}'
+        return os.path.join(path, filename)
+    return wrapper
+
+
+class Prediction(models.Model):
+    image = models.ImageField(blank=True, null=True, upload_to=rename_image('upload/images/'))
+    landmark = models.CharField(blank=True, null=True, max_length=255)
+    probability = models.FloatField(blank=True, null=True)
