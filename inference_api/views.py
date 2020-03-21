c37da1b58c59f89028e595a76fd99d2a9471b3a7
Milan <milan_susa@hotmail.com>
2020-03-21 18:53:15 +0100

    Add APIView for Prediction model.

--- inference_api/views.py
+++ inference_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from inference_api.serializers import PredictionSerializer
 
-# Create your views here.
+
+class PredictionView(APIView):
+
+    def post(self, request):
+        prediction_serializer = PredictionSerializer(data=request.data)
+
+        if prediction_serializer.is_valid():
+            prediction_serializer.save()
+            return Response(data=prediction_serializer.data,
+                            status=status.HTTP_201_CREATED)
+
+        return Response(data=prediction_serializer.errors,
+                        status=status.HTTP_400_BAD_REQUEST)
