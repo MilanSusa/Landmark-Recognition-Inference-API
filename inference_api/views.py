8e44b331fff596a25e8a6df1560ad904b6da9b71
Milan <milan_susa@hotmail.com>
2020-03-22 12:55:14 +0100

    Populate landmark and probability fields after inference.

--- inference_api/views.py
+++ inference_api/views.py
@@ -1,10 +1,12 @@
+import numpy as np
+import re
 from keras.applications.resnet50 import preprocess_input
 from keras_preprocessing.image import ImageDataGenerator
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from inference_api.serializers import PredictionSerializer
-from landmark_recognition.settings import MODEL
+from landmark_recognition.settings import MODEL, LANDMARK_ID_DF, TRAIN_DF
 
 
 class PredictionView(APIView):
@@ -22,6 +24,30 @@ class PredictionView(APIView):
 
             acc = MODEL.predict_generator(generator=test_data_gen,
                                           steps=1)
+
+            index_of_maximum = np.argmax(acc[len(acc) - 1])
+            landmark_id = LANDMARK_ID_DF.iloc[index_of_maximum]['landmark']
+            landmark_urls = TRAIN_DF.loc[TRAIN_DF['landmark_id'] == landmark_id]['url']
+
+            annotated_landmark_names = {}
+            for url in landmark_urls:
+                landmark_name_with_jpg_extension = url.rpartition('/')[2]
+                landmark_name_without_jpg_extension = landmark_name_with_jpg_extension.split('.')[0]
+                landmark_name_with_numbers = ' '.join(landmark_name_without_jpg_extension.split('_')[0:-1])
+                landmark_name_with_excess_spaces = re.sub('%[0-9a-zA-Z]+', ' ', landmark_name_with_numbers)
+                landmark_name = re.sub(' +', ' ', landmark_name_with_excess_spaces)
+
+                if landmark_name in annotated_landmark_names:
+                    annotated_landmark_names[landmark_name] += 1
+                else:
+                    annotated_landmark_names[landmark_name] = 1
+
+            most_probable_landmark = max(annotated_landmark_names, key=lambda k: annotated_landmark_names[k])
+            probability = np.max(acc[len(acc) - 1])
+
+            prediction_serializer.validated_data['landmark'] = most_probable_landmark
+            prediction_serializer.validated_data['probability'] = probability
+            prediction_serializer.save()
 
             return Response(data=prediction_serializer.data,
                             status=status.HTTP_201_CREATED)
